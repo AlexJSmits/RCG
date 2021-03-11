97e4b0804169c5cf14788ec0fc9471f5efa1d382
Caerwyn <c_barts@live.com>
2021-03-11 16:52:05 +1100

    Main Menu Updates

---
+++ RCG_UnityProject/Assets/Scripts/ParticleColourRandomizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleColourRandomizer : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        var main = ps.main;
+        main.startColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+    }
+}
