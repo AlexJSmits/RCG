609e68c9495d601865e47b08f8c340e8f2eae1bc
Alex Smits <alexsmits247@gmail.com>
2021-03-15 17:11:43 +1100

    Script Architecture Tweak
    I made it possible to change the controls through unity events. I was also messaging around with new art tiles - hence the large amount of changes.

---
+++ RCG_UnityProject/Assets/Scripts/ColliderEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ColliderEvent : MonoBehaviour
+{
+    public UnityEvent EnterEvent;
+    public UnityEvent ExitEvent;
+
+    void OnTriggerEnter2D()
+    {
+         EnterEvent.Invoke();
+    }
+
+    void OnTriggerExit2D()
+    {
+         ExitEvent.Invoke();
+    }
+}
