077ce397a9cc21a4136a36a253127b3a672706be
Alex <Smits>
2021-03-18 11:11:04 +1100

    Scene Change Script

---
+++ RCG_UnityProject/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    
+    public int num;
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:num);
+    }
+}
