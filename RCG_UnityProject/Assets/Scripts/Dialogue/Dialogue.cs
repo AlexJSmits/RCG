3e18303ec62b310a0f3697af0ae45d4ae432b8a7
Alex Smits <alexsmits247@gmail.com>
2021-03-20 21:41:58 +1100

    first pass of dialogue system
    I still want to streamline the text input system and add polish (i.e. letters appearing individually, public float for time is active)

---
+++ RCG_UnityProject/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshPro text;
+    public string textToShow;
+    public Animator animator;
+
+    public void DialoguePopUp()
+    {
+        animator.SetBool("textOpen", true);
+        text.text = textToShow;
+        Invoke("DialoguePopDown", 6f);
+    }
+
+    void DialoguePopDown()
+    {
+        animator.SetBool("textOpen", false);
+    }
+}
