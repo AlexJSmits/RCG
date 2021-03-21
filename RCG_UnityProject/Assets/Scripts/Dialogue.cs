ad0af49755a68544cfa94a69223c27622d8b897c
Chloe <crobson011@outlook.com>
2021-03-21 20:14:43 +1100

    Alex - Updated Dialogue System with smooth animation and duration float

--- RCG_UnityProject/Assets/Scripts/Dialogue.cs
+++ RCG_UnityProject/Assets/Scripts/Dialogue.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
-    public TextMeshPro text;
-    public string textToShow;
+    public TextMeshPro Text;
     public Animator animator;
+
+    [TextArea(3, 10)]
+    public string textToShow;
+
+    public float duration;
 
     public void DialoguePopUp()
     {
         animator.SetBool("textOpen", true);
-        text.text = textToShow;
-        Invoke("DialoguePopDown", 6f);
+        Text.text = textToShow;
+        Invoke("DialoguePopDown", duration);
     }
 
     void DialoguePopDown()
     {
         animator.SetBool("textOpen", false);
+        Invoke("DialogueHide", 0.5f);
     }
+
+    void DialogueHide()
+    {
+        animator.SetTrigger("dialogueHasFinished");
+    }
+
 }
