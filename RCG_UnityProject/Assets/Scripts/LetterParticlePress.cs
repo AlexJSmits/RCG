6f39452cba050bd4ed0db5a1303addc68375dca7
Barty <c_barts@live.com>
2021-03-23 20:55:10 +1100

    Commented out debug.log and fixed error for missing instantiated object

--- RCG_UnityProject/Assets/Scripts/LetterParticlePress.cs
+++ RCG_UnityProject/Assets/Scripts/LetterParticlePress.cs
@@ -18,10 +18,19 @@ public class LetterParticlePress : MonoBehaviour
             {
                 if (Input.GetKeyDown(keyCode))
                 {
-                    Debug.Log("KeyCode down: " + keyCode);
+                    //Debug.Log("KeyCode down: " + keyCode);
                     fileName = keyCode + "-LetterParticle";
                     UnityEngine.Object loadedObject = Resources.Load("LetterPrefabs/" + fileName);
-                    Instantiate((GameObject)loadedObject, new Vector3(transform.position.x,transform.position.y,transform.position.z), Quaternion.Euler(0, 180, 0), instantiateObject);
+                    string stringToCheck = fileName;
+                    string[] stringArray = { "A-LetterParticle", "B-LetterParticle", "C-LetterParticle", "D-LetterParticle", "E-LetterParticle", "F-LetterParticle", "G-LetterParticle", "H-LetterParticle", "I-LetterParticle", "J-LetterParticle", "K-LetterParticle", "L-LetterParticle", "M-LetterParticle", "N-LetterParticle", "O-LetterParticle", "P-LetterParticle", "Q-LetterParticle", "R-LetterParticle", "S-LetterParticle", "T-LetterParticle", "U-LetterParticle", "V-LetterParticle", "W-LetterParticle", "X-LetterParticle", "Y-LetterParticle", "Z-LetterParticle", "Space-LetterParticle"};
+                    foreach (string x in stringArray)
+                    {
+                        if (stringToCheck.Contains(x))
+                        {
+                            Instantiate((GameObject)loadedObject, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler(0, 180, 0), instantiateObject);
+                            break;
+                        }
+                    }
                     break;
                 }
             }
