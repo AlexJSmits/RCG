8b90df1e99d206a7e40189e97e0cf752cee24d71
Barty <c_barts@live.com>
2021-03-25 17:38:14 +1100

    Updates to main menu

---
+++ RCG_UnityProject/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void ExitGameTrigger()
+    {
+        Application.Quit();
+        //Debug.Log("Game Quit");
+    }
+}
