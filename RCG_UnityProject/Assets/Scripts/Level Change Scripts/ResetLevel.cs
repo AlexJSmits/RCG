9de07c1bce1ce9c1791fccc413c14f8579ce1872
Caerwyn <c_barts@live.com>
2021-04-08 16:56:03 +1000

    Finalized Script for level reset

--- RCG_UnityProject/Assets/Scripts/Level Change Scripts/ResetLevel.cs
+++ RCG_UnityProject/Assets/Scripts/Level Change Scripts/ResetLevel.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ResetLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+
+    private float countdown;
+    private int levelNumber;
+
+    void Awake()
     {
-        
+        countdown = 2.15f;
+        levelNumber = SceneManager.GetActiveScene().buildIndex;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Backspace))
+        {
+            countdown -= Time.deltaTime;
+            animator.SetBool("Countdown", true);
+        }
+        else
+        {
+            countdown = 2.15f;
+            animator.SetBool("Countdown", false);
+        } 
+
+        if (countdown <= 0.0f)
+        {
+            SceneManager.LoadScene(sceneBuildIndex: levelNumber);
+        }
     }
 }
